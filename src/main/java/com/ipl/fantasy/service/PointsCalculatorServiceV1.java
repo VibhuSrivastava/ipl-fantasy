b469701a02de51611bda5b1579fe886b25130dea
Vibhu Srivastava <vibhu.srivastava@tink.com>
2021-04-18 23:13:34 +0200

    add pojos

--- src/main/java/com/ipl/fantasy/service/PointsCalculatorServiceV1.java
+++ src/main/java/com/ipl/fantasy/service/PointsCalculatorServiceV1.java
@@ -1,6 +1,7 @@
 package com.ipl.fantasy.service;
 
 
+import com.google.gson.Gson;
 import com.ipl.fantasy.api.PointsRequest;
 import com.ipl.fantasy.api.PointsResponse;
 import com.ipl.fantasy.domain.PointsCalculator;
@@ -16,6 +17,8 @@ import javax.ws.rs.core.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
+import org.jetbrains.annotations.NotNull;
 
 @Path("/")
 @Singleton
@@ -41,7 +44,7 @@ public class PointsCalculatorServiceV1 {
     @Produces(MediaType.APPLICATION_JSON)
     public String scoreBoard() throws IOException {
 
-        OkHttpClient client = new OkHttpClient();
+        OkHttpClient client = getOkHttpClient();
 
         Request request = new Request.Builder()
             .url("https://dev132-cricket-live-scores-v1.p.rapidapi.com/scorecards.php?seriesid=2780&matchid=50809")
@@ -50,15 +53,9 @@ public class PointsCalculatorServiceV1 {
             .addHeader("x-rapidapi-host", "dev132-cricket-live-scores-v1.p.rapidapi.com")
             .build();
 
-        Response response = client.newCall(request).execute();
-        System.out.println(request);
-        try {
+        Response response;
             response = client.newCall(request).execute();
             System.out.println(response);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
 
         return response.body().string();
     }
@@ -69,7 +66,7 @@ public class PointsCalculatorServiceV1 {
     @Produces(MediaType.APPLICATION_JSON)
     public String series() throws IOException {
 
-        OkHttpClient client = new OkHttpClient();
+        OkHttpClient client = getOkHttpClient();
 
         Request request = new Request.Builder()
             .url("https://dev132-cricket-live-scores-v1.p.rapidapi.com/matchseries.php?seriesid=2780")
@@ -81,5 +78,31 @@ public class PointsCalculatorServiceV1 {
         return response.body().string();
     }
 
+    @GET
+    @Path("/v1/allplayerpoints")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String allPlayerPoints() throws IOException {
+
+        OkHttpClient client = getOkHttpClient();
+
+        Request request = new Request.Builder()
+            .url("https://dev132-cricket-live-scores-v1.p.rapidapi.com/scorecards.php?seriesid=2780&matchid=50809")
+            .get()
+            .addHeader("x-rapidapi-key", "dc1d36d44dmsh35e298098fcc16dp1317bfjsn530fdb81dab5")
+            .addHeader("x-rapidapi-host", "dev132-cricket-live-scores-v1.p.rapidapi.com")
+            .build();
+
+        Gson gson = new Gson();
+        ResponseBody responseBody = client.newCall(request).execute().body();
+        //SimpleEntity entity = gson.fromJson(responseBody.string(), SimpleEntity.class);
+
+        return responseBody.string();
+    }
+
+    @NotNull
+    private OkHttpClient getOkHttpClient() {
+        return new OkHttpClient();
+    }
 
 }
